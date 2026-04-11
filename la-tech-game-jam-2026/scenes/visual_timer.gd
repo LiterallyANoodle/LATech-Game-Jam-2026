62fdf11b00e52592bd4f7a47d52c385590818d46
Matthew Mahan <themaskedkid321@gmail.com>
2026-04-11 00:45:50 -0400

    timer in progress

---
+++ la-tech-game-jam-2026/scenes/visual_timer.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready timer: Timer = $Timer
+@onready demon_follower: PathFollow2D = $Path2D/PathFollow2D
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
