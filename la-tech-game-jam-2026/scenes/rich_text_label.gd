488da29e4c2fee42155aec57dda520e09a4e23c0
CupidTheDead <bubwalters@gmail.com>
2026-04-10 22:26:30 -0700

    log begin

---
+++ la-tech-game-jam-2026/scenes/rich_text_label.gd
@@ -0,0 +1,20 @@
+extends RichTextLabel
+
+
+func _ready() -> void:
+	
+	# Clear
+	text = ""
+	SignalBus.log_event.connect(_on_log_event)
+	
+	# Init message
+	_on_log_event("Game Start.") 
+	
+func _on_log_event(message: String) -> void:
+	# Add a new line with the message and add ">" to begining of message
+	# [color] tags work here if you enable 'BBCode Enabled' in the inspector
+	append_text("\n> " + message)
+	
+	# Autoscroll to the bottom so we see the newest message
+	var scrollbar: VScrollBar = get_v_scroll_bar()
+	scrollbar.value = scrollbar.max_value
--- la-tech-game-jam-2026/scenes/signal_bus.gd
+++ la-tech-game-jam-2026/scenes/signal_bus.gd
@@ -1,3 +1,5 @@
 extends Node
 
 signal PLAYER_MOVED(direction: Vector2)
+
+signal log_event(message: String)
