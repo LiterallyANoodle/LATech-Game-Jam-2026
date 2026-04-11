91c27498e327a9312bb07b7e0b0069d3b6ee04a7
Matthew Mahan <themaskedkid321@gmail.com>
2026-04-11 12:32:38 -0400

    element textures

--- la-tech-game-jam-2026/constants.gd
+++ la-tech-game-jam-2026/constants.gd
@@ -7,7 +7,17 @@ const GRID_SIZE: int = 64
 const MOVE_TIME: float = 0.10
 
 # Atom textures
-var air_tex: Texture2D = load("res://.godot/imported/poopy_air.png-39d0e8ba2a99cdbdd97878c876e7c6bc.ctex")
-var fire_tex: Texture2D = load("res://.godot/imported/poopy_atom.png-1dfad63143ba100ae804eb975d0efbab.ctex")
-var earth_tex: Texture2D = load("res://.godot/imported/poopy_earth.png-3641b60a6dc4ce0382ee94705ac8c906.ctex")
-var water_tex: Texture2D = load("res://.godot/imported/poopy_water.png-326f2f47c33d16830bc64acd827fda9d.ctex")
+@onready var air_tex: AtlasTexture = AtlasTexture.new()
+@onready var fire_tex: AtlasTexture = AtlasTexture.new()
+@onready var earth_tex: AtlasTexture = AtlasTexture.new()
+@onready var water_tex: AtlasTexture = AtlasTexture.new()
+
+func _ready() -> void:
+	air_tex.set_atlas(load("res://assets/Elements.png"))
+	air_tex.region = Rect2(48, 0, 16, 16)
+	fire_tex.set_atlas(load("res://assets/Elements.png"))
+	fire_tex.region = Rect2(0, 0, 16, 16)
+	earth_tex.set_atlas(load("res://assets/Elements.png"))
+	earth_tex.region = Rect2(16, 0, 16, 16)
+	water_tex.set_atlas(load("res://assets/Elements.png"))
+	water_tex.region = Rect2(32, 0, 16, 16)
