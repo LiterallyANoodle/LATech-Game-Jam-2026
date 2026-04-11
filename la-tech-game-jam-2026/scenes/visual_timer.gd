1b9040fd7d583908167dcd6f1bcf1c359ac319d6
Matthew Mahan <themaskedkid321@gmail.com>
2026-04-11 14:39:00 -0400

    visual timer

--- la-tech-game-jam-2026/scenes/visual_timer.gd
+++ la-tech-game-jam-2026/scenes/visual_timer.gd
@@ -2,9 +2,18 @@ extends SubViewport
 
 @onready var timer: Timer = $Timer
 @onready var demon_follower: PathFollow2D = $Path2D/PathFollow2D
+@onready var demon: Sprite2D = $Path2D/PathFollow2D/Demon
+
+@onready var progress_ratio: float = 0.0
+
+@export var bounce_height: float = 40.0
+@export var bounce_frequency: float = 100.0
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	demon_follower.progress_ratio = 1 - (timer.time_left / constants.ROUND_TIME)
+	progress_ratio = 1 - (timer.time_left / constants.ROUND_TIME)
+	demon_follower.progress_ratio = progress_ratio
+	demon.global_position.y = demon_follower.global_position.y - \
+		(bounce_height * abs(sin(progress_ratio * bounce_frequency)))
